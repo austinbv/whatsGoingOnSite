3c14a91b929a8e7bb69e08efa5a0f4832ae4fa8b
Austin <austinbv@gmail.com>
2012-03-24 18:47:38 -0600

    lets go vertical!

--- javascripts/CalApp.js
+++ javascripts/CalApp.js
@@ -1,71 +1,101 @@
-window.CalApp = {
-  start:function () {
-    CalApp.Router = new CalApp.AppRouter();
-    Backbone.history.start();
-  },
+(function () {
+  "use strict";
 
-  API_KEY:'AIzaSyA1y3uKOlHCBg9oQmUO1XYTjBZ9M37UIu8'
-};
+  window.CalApp = {
+    start:function () {
+      CalApp.Router = new CalApp.AppRouter();
+      Backbone.history.start();
 
-CalApp.AppRouter = Backbone.Router.extend({
-  routes:{
-    'state':'retouter',
-    'login':'login',
-    '*catchAll':'rerouter'
-
-  },
-
-  rerouter:function () {
-    if (window.location.hash.length > 0) {
-      sessionStorage.setItem('accessToken', this._parseQuery()['access_token'].toString());
-      CalApp.Router.navigate('', {trigger:true})
-    } else if (sessionStorage.getItem('accessToken') && sessionStorage.getItem('accessToken').length > 0) {
-      $.ajax({
-        url:"https://www.googleapis.com/oauth2/v1/tokeninfo",
-        data:{
-          access_token:sessionStorage.getItem('accessToken'),
-        },
-        method:'GET',
-        dataType:'jsonp',
-
-        error:function () {
-          CalApp.Router.navigate('login', {trigger:true})
-        },
-
-        success:function () {
-          new CalApp.Views.HeaderView();
-          new CalApp.Views.CurrentTime();
-        }
+      $('#calendar-picker-wrap').click(function () {
+        $('#calendar-picker').trigger('click');
       });
 
-    } else {
-      CalApp.Router.navigate('login', {trigger:true})
+      CalApp.Helpers.resize();
+
+      $(window).resize(function () {
+        CalApp.Helpers.resize();
+      });
+    },
+
+    API_KEY:'AIzaSyA1y3uKOlHCBg9oQmUO1XYTjBZ9M37UIu8',
+    POLLING_INTERVAL:1000 * 60 * 10
+  };
+
+  CalApp.Helpers = {
+    resize:function () {
+      $('#calendar').height($(window).height() - 80);
+    },
+
+    save_state:function (element) {
+      var $element = $(element);
+      sessionStorage.setItem('currentCalendar', $element.val());
+    },
+
+    save_state_and_login:function (element) {
+      var $element = $(element);
+      CalApp.Helpers.save_state(element);
+//    CalApp.Router.navigate('login', {trigger:true});
     }
-  },
+  };
 
-  login:function () {
-    var queryString = {
-      scope:'https://www.googleapis.com/auth/calendar.readonly',
-      state:'ok',
-      redirect_uri:window.location.origin + window.location.pathname,
-      response_type:'token',
-      client_id:'533339277613.apps.googleusercontent.com'
-    };
+  CalApp.AppRouter = Backbone.Router.extend({
+    routes:{
+      'login':'login',
+      '*catchAll':'rerouter'
+    },
 
-    window.location = 'https://accounts.google.com/o/oauth2/auth?' + $.param(queryString)
-  },
+    rerouter:function () {
+      if (window.location.hash.length > 0) {
+        sessionStorage.setItem('accessToken', this._parseQuery().access_token.toString());
+        CalApp.Router.navigate('', {trigger:true});
 
-  _parseQuery:function () {
-    var result = {}, queryString = location.hash.substring(1),
-      re = /([^&=]+)=([^&]*)/g, m;
+      } else if (sessionStorage.getItem('accessToken') && sessionStorage.getItem('accessToken').length > 0) {
+        $.ajax({
+          url:"https://www.googleapis.com/oauth2/v1/tokeninfo",
+          data:{
+            access_token:sessionStorage.getItem('accessToken')
+          },
+          method:'GET',
+          dataType:'jsonp',
 
-    while (m = re.exec(queryString)) {
-      result[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+          error:function () {
+            CalApp.Helpers.save_state_and_login($('#calendar-picker'));
+          },
+
+          success:function () {
+            new CalApp.Views.HeaderView();
+//          new CalApp.Views.CurrentTime();
+          }
+        });
+
+      } else {
+        CalApp.Helpers.save_state_and_login($('#calendar-picker'));
+      }
+    },
+
+    login:function () {
+      var queryString = {
+        scope:'https://www.googleapis.com/auth/calendar.readonly',
+        state:'ok',
+        redirect_uri:window.location.origin + window.location.pathname,
+        response_type:'token',
+        client_id:'533339277613.apps.googleusercontent.com'
+      };
+
+      window.location = 'https://accounts.google.com/o/oauth2/auth?' + $.param(queryString);
+    },
+
+    _parseQuery:function () {
+      var result = {}, queryString = location.hash.substring(1),
+        re = /([^&=]+)=([^&]*)/g, m;
+
+      while (m = re.exec(queryString)) {
+        result[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+      }
+
+      return result;
     }
-
-    return result;
-  }
-}),
+  });
 
   CalApp.Models = {
     Calendar:Backbone.Model.extend({
@@ -73,7 +103,7 @@ CalApp.AppRouter = Backbone.Router.extend({
         return {
           calendarId:item.id,
           calendarName:item.summary
-        }
+        };
       }
     }),
 
@@ -108,23 +138,24 @@ CalApp.AppRouter = Backbone.Router.extend({
           startTime:new Date(Date.parse(item.start.dateTime || item.start.date)),
           endTime:new Date(Date.parse(item.end.dateTime || item.start.date)),
           meetingRoom:item.location
-        }
+        };
       },
 
       startDistance:function () {
         var startTime = this.get('startTime');
         var minutesUsed = (startTime.getHours() - 8) * 60 + startTime.getMinutes();
 
-        var percentUsed = minutesUsed / (9.0 * 60);
-        var offset = percentUsed * $('body').width();
+        var percentUsed = minutesUsed / (9 * 60);
+        var offset = percentUsed * $('#calendar').outerHeight();
 
         return offset;
       },
 
-      calculateWidth:function () {
+      calculateHeight:function () {
         var lengthOfMeetingSeconds = (this.get('endTime').getTime() - this.get('startTime').getTime()),
           lengthOfMeetingsMinutes = lengthOfMeetingSeconds / (1000 * 60);
-        return  (lengthOfMeetingsMinutes / (9 * 60)) * $('body').width();
+
+        return  Math.floor((lengthOfMeetingsMinutes / (9 * 60)) * $('#calendar').height());
       }
     }),
 
@@ -142,7 +173,7 @@ CalApp.AppRouter = Backbone.Router.extend({
 
         setInterval(function () {
           that._setDates();
-        }, 1000)
+        }, CalApp.POLLING_INTERVAL);
       },
 
       _setDates:function () {
@@ -154,191 +185,205 @@ CalApp.AppRouter = Backbone.Router.extend({
         this.set('dayOfMonth', date.getDate());
         this.set('month', this.months[date.getMonth()]);
 
-        return this
+        return this;
       }
     })
   };
 
-CalApp.Collections = {
-  Calendars:Backbone.Collection.extend({
-    model:CalApp.Models.Calendar,
-    url:function () {
-      var baseUrl = 'https://www.googleapis.com/calendar/v3/users/me/calendarList?',
-        data = {
-          pp:1,
-          key:CalApp.API_KEY,
-          access_token:sessionStorage.getItem('accessToken')
-        },
-        callback = "&callback=?"
+  CalApp.Collections = {
+    Calendars:Backbone.Collection.extend({
+      model:CalApp.Models.Calendar,
 
-      return baseUrl + $.param(data) + callback
-    },
+      events:{
 
-    parse:function (calendars) {
-      if (calendars.error) {
-        CalApp.Router.navigate('login', {trigger: true});
+      },
+
+      url:function () {
+        var baseUrl = 'https://www.googleapis.com/calendar/v3/users/me/calendarList?',
+          data = {
+            pp:1,
+            key:CalApp.API_KEY,
+            access_token:sessionStorage.getItem('accessToken')
+          },
+          callback = "&callback=?";
+
+        return baseUrl + $.param(data) + callback;
+      },
+
+      parse:function (calendars) {
+        if (calendars.error) {
+          CalApp.Helpers.save_state_and_login($('#calendar-picker'));
+        }
+        return calendars.items;
       }
-      return calendars.items
-    }
-  }),
+    }),
 
-  Meetings:Backbone.Collection.extend({
-    model:CalApp.Models.Meeting,
-    url:function () {
-      return this._buildURL(new Date());
-    },
+    Meetings:Backbone.Collection.extend({
+      model:CalApp.Models.Meeting,
+      url:function () {
+        return this._buildURL(new Date());
+      },
 
-    events:{
-      'add':'CalApp.MeetingIndex'
-    },
+      events:{
+        'add':'CalApp.MeetingIndex'
+      },
 
-    comparator:function (meeting) {
-      return meeting.get('startTime');
-    },
+      comparator:function (meeting) {
+        return meeting.get('startTime');
+      },
 
-    parse:function (results) {
-
-      if (results.error) {
-        CalApp.Router.navigate('login', {trigger: true});
-      }
-      return _.reject(results.items, function (item) {
-        return item.status == 'cancelled'
-      });
-    },
-
-    _buildURL:function (date) {
-      var today = new Date(date.getFullYear(), date.getMonth(), date.getDate()),
-        tomorrow = new Date(today.getTime() + (1000 * 60 * 60 * 24)),
-        baseUrl = 'https://www.googleapis.com/calendar/v3/calendars',
-        calendar = $('#calendar-picker').val(),
-        rest = 'events',
-        queryString = {
-          timeMax:tomorrow.toISOString(),
-          timeMin:today.toISOString(),
-          pp:100,
-          key:CalApp.APIKEY,
-          access_token:sessionStorage.getItem('accessToken')
+      parse:function (results) {
+        if (results.error) {
+          CalApp.Helpers.save_state_and_login($('#calendar-picker'));
         }
-
-      return baseUrl + '/' + calendar + '/' + rest + '?' + $.param(queryString) + '&callback=?';
-    }
-  })
-}
-CalApp.Views = {
-  CalendarsSelectView:Backbone.View.extend({
-    el: '#calendar-picker',
-    events: {
-      'change': 'renderMeetings'
-    },
-    initialize:function () {
-      this.collection = new CalApp.Collections.Calendars();
-
-      this.collection.on('all', this.render, this);
-      this.collection.fetch({
-        error:function () {
-          CalApp.Router.navigate('login', {trigger:true})
-        }
-      });
-    },
-
-    render:function () {
-      var template = _.template($('#calendar-options-template').html());
-      this.collection.each(function (calendar) {
-        $(this.el).append(template(calendar.toJSON()));
-      }, this);
-
-      this
-    },
-
-    renderMeetings: function(calendar) {
-      CalApp.meetings = new CalApp.Views.MeetingView();
-
-    }
-  }),
-
-  MeetingView:Backbone.View.extend({
-    el:'#calendar',
-
-    initialize:function () {
-      var that = this;
-      this.collection = new CalApp.Collections.Meetings();
-
-      this.collection.on('all', this.render, this);
-      this.collection.fetch({
-        error:function () {
-          CalApp.Router.navigate('login', {trigger:true});
-        }
-      });
-      this.render();
-
-      setInterval(function () {
-        that.collection.fetch();
-          that.render();
-      }, 1000);
-    },
-
-    render:function () {
-      var template = _.template($('#meeting-template').html());
-
-      $('.event').remove();
-      this.collection.each(function (meeting) {
-        $(this.el).append(template(meeting.toJSON()));
-        $(this.el).children('.event:last').css({
-          position:'absolute',
-          height:'60%',
-          top:'10%',
-          left:meeting.startDistance() + 'px',
-          width:meeting.calculateWidth() + 'px'
+        return _.reject(results.items, function (item) {
+          return item.status === 'cancelled';
         });
-      }, this);
-      return this;
-    }
-  }),
+      },
 
-  HeaderView:Backbone.View.extend({
-    el:'#header',
+      _buildURL:function (date) {
+        var today = new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+          tomorrow = new Date(today.getTime() + (1000 * 60 * 60 * 24)),
+          baseUrl = 'https://www.googleapis.com/calendar/v3/calendars',
+          calendar = $('#calendar-picker').val(),
+          rest = 'events',
+          queryString = {
+            timeMax:tomorrow.toISOString(),
+            timeMin:today.toISOString(),
+            pp:100,
+            key:CalApp.APIKEY,
+            access_token:sessionStorage.getItem('accessToken')
+          };
 
-    initialize:function () {
-      this.model = new CalApp.Models.Clock();
-      this.model.on('all', this.render, this);
-      this.render();
-      new CalApp.Views.CalendarsSelectView();
-    },
+        return baseUrl + '/' + encodeURIComponent(calendar) + '/' + rest + '?' + $.param(queryString) + '&callback=?';
+      }
+    })
+  };
 
-    render:function () {
-      var template = _.template($('#header-template').html());
-      $(this.el).html(template(this.model.toJSON()));
-      this._setFontSize();
-      return this;
-    },
+  CalApp.Views = {
+    CalendarsSelectView:Backbone.View.extend({
+      el:'#calendar-picker',
+      events:{
+        'change':'renderMeetings'
+      },
+      initialize:function () {
+        var that = this;
+        this.collection = new CalApp.Collections.Calendars();
 
-    _setFontSize:function () {
-      $('#date').css({fontSize:$('#date').innerHeight()});
-    }
-  }),
+        this.collection.on('all', this.render, this);
+        this.collection.fetch({
+          success:function () {
+            $(that.el).trigger('change');
+          },
 
-  CurrentTime:Backbone.View.extend({
-    el:'#current-time-wrapper',
+          error:function () {
+            CalApp.Helpers.save_state_and_login($('#calendar-picker'));
+          }
+        });
 
-    initialize:function () {
-      this.model = new CalApp.Models.Clock();
+        $(window).resize(function () {
+          $(that.el).trigger('change');
+        });
+      },
 
-      this.model.on('all', this.render, this);
-      this.render();
-    },
+      render:function () {
+        var template = _.template($('#calendar-options-template').html());
+        this.collection.each(function (calendar) {
+          $(this.el).append(template(calendar.toJSON()));
+        }, this);
+        $(this.el).val(sessionStorage.getItem('currentCalendar'));
 
-    render:function () {
-      var template = _.template($('#current-time').html());
-      $(this.el).html(template({offset:this.calculateOffset()}));
+        return this;
+      },
 
-      return this;
-    },
+      renderMeetings:function (calendar) {
+        CalApp.Helpers.save_state(this.el);
+        CalApp.meetings = new CalApp.Views.MeetingView();
+      }
+    }),
 
-    calculateOffset:function () {
-      var secondsIntoTheDay = ((this.model.get('hour') - 8) * 60 * 60) + (this.model.get('minute') * 60) + (this.model.get('second'));
+    MeetingView:Backbone.View.extend({
+      el:'#calendar',
 
-      var amountOfDayUsed = secondsIntoTheDay / (60 * 60 * 9)
-      return $('body').width() * amountOfDayUsed
-    }
-  })
-};
+      initialize:function () {
+        var that = this;
+        this.collection = new CalApp.Collections.Meetings();
+
+        this.collection.on('all', this.render, this);
+        this.collection.fetch({
+          error:function (x) {
+            CalApp.Helpers.save_state_and_login($('#calendar-picker'));
+          }
+        });
+        this.render();
+
+        setInterval(function () {
+          that.collection.fetch();
+          that.render();
+        }, CalApp.POLLING_INTERVAL);
+      },
+
+      render:function () {
+        var template = _.template($('#meeting-template').html());
+
+        $('.meeting').remove();
+        this.collection.each(function (meeting) {
+          $(this.el).append(template(meeting.toJSON()));
+          $(this.el).children('.meeting:last').css({
+            position:'absolute',
+            top:(meeting.startDistance()) + 'px',
+            height:meeting.calculateHeight() + 'px'
+          });
+        }, this);
+        return this;
+      }
+    }),
+
+    HeaderView:Backbone.View.extend({
+      el:'#header',
+
+      initialize:function () {
+        this.model = new CalApp.Models.Clock();
+        this.model.on('all', this.render, this);
+        this.render();
+        new CalApp.Views.CalendarsSelectView();
+      },
+
+      render:function () {
+        var template = _.template($('#header-template').html());
+        $(this.el).html(template(this.model.toJSON()));
+        this._setFontSize();
+        return this;
+      },
+
+      _setFontSize:function () {
+        $('#date, #calendar-picker').css({fontSize:$('#date').innerHeight()});
+      }
+    }),
+
+    CurrentTime:Backbone.View.extend({
+      el:'#current-time-wrapper',
+
+      initialize:function () {
+        this.model = new CalApp.Models.Clock();
+
+        this.model.on('all', this.render, this);
+        this.render();
+      },
+
+      render:function () {
+        var template = _.template($('#current-time').html());
+        $(this.el).html(template({offset:this.calculateOffset()}));
+
+        return this;
+      },
+
+      calculateOffset:function () {
+        var secondsIntoTheDay = ((this.model.get('hour') - 8) * 60 * 60) + (this.model.get('minute') * 60) + (this.model.get('second'));
+
+        var amountOfDayUsed = secondsIntoTheDay / (60 * 60 * 9);
+        return $('#calendar').height() * amountOfDayUsed;
+      }
+    })
+  };
+})();
